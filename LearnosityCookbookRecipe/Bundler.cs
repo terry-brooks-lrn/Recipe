ab38105ac214a681e7d86c5f745b73449ae1c8ce
Terry Arthur Brooks, Jr <Terry.Arthur@BrooksJr.com>
2024-01-24 11:32:34 -0500

    Update Bundler.cs

--- LearnosityCookbookRecipe/Bundler.cs
+++ LearnosityCookbookRecipe/Bundler.cs
@@ -1,11 +1,22 @@
-﻿using System;
+using System;
+
 namespace LearnosityCookbookRecipe
 {
 	public class Bundler
 	{
-		public Bundler()
+        // To Limit On Network Traffic This Class Will Bundle All Seraliazed JSON into Packets of 50 (The Max Per Request) 
+        private List<string> jsonList = new List<string>();
+
+        public string[] Bundle(string json)
 		{
-		}
-	}
+            if (jsonList.Count < 50)
+            {
+               jsonList.Add(json);
+
+            }
+            return jsonList.ToArray();
+
+        }
+    }
 }
 
